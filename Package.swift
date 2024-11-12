d95608087f7672a26200665cb6de192885a817bb
Alexander Kraev <lexkraev@gmail.com>
2024-11-12 23:59:34 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,12 +13,20 @@ let package = Package(
             targets: ["YooKassaPayments"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/appmetrica/appmetrica-sdk-ios", .upToNextMajor(from: "5.8.0")),
     ],
     targets: [
         .binaryTarget(
-            name: "YooKassaPayments",
+            name: "YooKassaPaymentsBinary",
             url: "https://github.com/c-villain/YooKassaPayments/releases/download/7.2.0/YooKassaPayments.xcframework.zip",
             checksum: "cda6fa8a0f7b768336de3ad39831f9e3ed4b6594f48493671aa998fdbb839c1f"
         ),
+        .target(
+            name: "YooKassaPayments",
+            dependencies: [
+                .target(name: "YooKassaPaymentsBinary"),
+                .product(name: "AppMetricaCore", package: "appmetrica-sdk-ios"),
+            ],
+        )
     ]
 )
