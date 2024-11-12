4d047431ec6a33adb2ae0d07b369b1636a8942ca
Alexander Kraev <aleksandr.kraev@lenta.com>
2024-11-13 01:23:07 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -10,23 +10,31 @@ let package = Package(
     products: [
         .library(
             name: "YooKassaPayments",
-            targets: ["YooKassaPayments"]),
+            targets: ["YooKassaPaymentsTarget"])
     ],
     dependencies: [
         .package(url: "https://github.com/appmetrica/appmetrica-sdk-ios", .upToNextMajor(from: "5.8.0")),
     ],
     targets: [
-        .binaryTarget(
-            name: "YooKassaPaymentsBinary",
-            url: "https://github.com/c-villain/YooKassaPayments/releases/download/7.2.0/YooKassaPayments.xcframework.zip",
-            checksum: "cda6fa8a0f7b768336de3ad39831f9e3ed4b6594f48493671aa998fdbb839c1f"
+        .target(
+            name: "YooKassaPaymentsTarget",
+            dependencies: [
+                .target(name: "YooKassaPaymentsWrapper")
+            ],
+            path: "SwiftPM-PlatformExclude/YooKassaPaymentsWrap"
         ),
         .target(
-            name: "YooKassaPayments",
+            name: "YooKassaPaymentsWrapper",
             dependencies: [
-                .target(name: "YooKassaPaymentsBinary"),
-                .product(name: "AppMetricaCore", package: "appmetrica-sdk-ios"),
+                .target(name: "YooKassaPayments"),
+                .product(name: "AppMetricaCore", package: "appmetrica-sdk-ios")
             ],
+            path: "YooKassaPaymentsWrapper"
+        ),
+        .binaryTarget(
+            name: "YooKassaPayments",
+            url: "https://github.com/c-villain/YooKassaPayments/releases/download/7.2.0/YooKassaPayments.xcframework.zip",
+            checksum: "cda6fa8a0f7b768336de3ad39831f9e3ed4b6594f48493671aa998fdbb839c1f"
         )
     ]
 )
